d857cb0736c14657d6ce82dc7ccb259ee359b68e
Jeffery <jefferyasamoahbless@gmail.com>
2026-03-02 10:20:54 +0000

    Fix: Added  testRuntimeOnly("org.junit.platform:junit-platform-launcher")

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,7 @@ dependencies {
 
     // Testing frameworks
     testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     testImplementation("org.testng:testng:7.8.0")
 
     // Logging
